e4b343dd37946343f0c7d80ffabb21989c3c92d9
Vijay-123-Jalla <vijaykumarrajujalla2@gmail.com>
2025-04-23 21:00:20 +0530

    Pushing the ecllipse project to central

---
+++ src/pushingtocentral/Vijay.java
@@ -0,0 +1,14 @@
+package pushingtocentral;
+
+public class Vijay {
+	public static void main(String[] args)
+	{
+		int a=10,b=20;
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(" a : "+a+"   b : "+b);
+				}
+
+}
